cf5dcc4355d3dbf3332ef592ea937d0d21268163
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-24 15:05:34 +0530

    Create team.js

---
+++ assests/team/team.js
@@ -0,0 +1,34 @@
+var rsfootballclub = {
+    name: 'RS Football Club',
+    shortName: 'RSFC',
+    stadium: 'RS City Stadium',
+    stadiumCapacity: 92632,
+    src: 'assests/team/rsfc.png'
+},
+annihilation = {
+    name: 'Annihilation',
+    shortName: 'ANN',
+    stadium: 'Vasio Club',
+    stadiumCapacity: 35519,
+    src: 'assests/team/anh.png'
+},
+ssfc = {
+    name: 'SSFC',
+    shortName: 'SSFC',
+    stadium: 'Laura Stadium',
+    stadiumCapacity: 92632,
+    src: 'assests/team/ssfc.png'
+};
+
+const teams = [
+    rsfootballclub, annihilation, ssfc
+];
+for (let i = 0; i < teams.length; i++) {
+   for (let j = 0; j < teams.length; j++) {
+        if (teams[i].name < teams[j].name) {
+            var temp = teams[i];
+            teams[i] = teams[j];
+            teams[j] = temp;
+        }
+    }
+}
