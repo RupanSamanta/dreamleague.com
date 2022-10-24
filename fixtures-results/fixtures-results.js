42d0e5de4050e364c5ea205a9f17ad1042237ce1
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-24 15:00:46 +0530

    Add files via upload

---
+++ fixtures-results/fixtures-results.js
@@ -0,0 +1,9 @@
+var fixtures = [
+    [rsfootballclub, ssfc],
+    [ssfc, annihilation],
+    [annihilation, rsfootballclub],
+    [ssfc, rsfootballclub],
+    [annihilation, ssfc],
+    [rsfootballclub, annihilation]
+],
+results = [];
