04e70dccd5dfe0823cf0a06426c4a4665b68feb8
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-25 15:13:12 +0530

    Add files via upload

---
+++ match/lineup.js
@@ -0,0 +1,63 @@
+var field = document.getElementById('field'),
+    button = document.querySelectorAll('#buttons button'),
+    stats_result = [
+        document.getElementById('potm'),
+        document.getElementById('lineups'),
+        document.getElementById('stats')
+    ];
+    for (let i = 0; i < button.length; i++)
+        button[i].onclick = function () {
+            openClose(i);
+        }
+    
+    function openClose(ind) {
+        for (let i = 0; i < stats_result.length; i++) 
+            if (i == ind) {
+                stats_result[i].style.display = 'block';
+                button[i].disabled = true;
+            }
+            else {
+                stats_result[i].style.display = 'none';
+                button[i].disabled = false;
+            }
+    }
+function printLineup(hf, hp, af, ap) {
+    var lineupBox = document.createElement('div');
+    lineupBox.id = 'home-lineup';
+    var div, p, img, span, count = 0;
+    for (let i = 0; i<hf.length; i++) {
+        div = document.createElement('div');
+        for (let j=0; j<hf[i]; j++) {
+            p = document.createElement('p');
+            img = document.createElement('img');
+            span = document.createElement('span');
+            img.src = hp[0][1];
+            span.innerHTML = hp[count][0];
+            p.appendChild(img);
+            p.appendChild(span);
+            div.appendChild(p);
+            count++;
+        }
+        lineupBox.appendChild(div);
+    }
+    field.appendChild(lineupBox);
+    lineupBox = document.createElement('div');
+    lineupBox.id = 'away-lineup';
+    count = 0;
+    for (let i = 0; i<af.length; i++) {
+        div = document.createElement('div');
+        for (let j=0; j<af[i]; j++) {
+            p = document.createElement('p');
+            img = document.createElement('img');
+            span = document.createElement('span');
+            img.src = ap[0][1];
+            span.innerHTML = ap[count][0];
+            p.appendChild(img);
+            p.appendChild(span);
+            div.appendChild(p);
+            count++;
+        }
+        lineupBox.appendChild(div);
+    }
+    field.appendChild(lineupBox);
+}
