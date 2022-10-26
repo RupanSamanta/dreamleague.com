efa8c8ed23a4096c9b2f6ff77d3fbc006775c6e3
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-26 15:36:40 +0530

    Add files via upload

---
+++ match/season-1/player-stats.js
@@ -0,0 +1,21 @@
+var player_goals = [
+    {
+        name: 'Phil Foden',
+        value: 1
+    }
+],
+player_assists = [
+    {
+        name: 'Kevin De Bryune',
+        value: 1
+    }
+],
+player_cleansheet = [
+    {
+        name: 'Ederson',
+        value: 1
+    }
+],
+season1_player_stats = [
+    player_goals, player_assists, player_cleansheet
+];
