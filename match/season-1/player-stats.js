d9079ee86c43c149d9f2f035ecc9233b6d99e610
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-26 20:51:26 +0530

    Update player-stats.js

--- match/season-1/player-stats.js
+++ match/season-1/player-stats.js
@@ -1,18 +1,24 @@
 var player_goals = [
     {
         name: 'Phil Foden',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Phil Foden.png',
         value: 1
     }
 ],
 player_assists = [
     {
         name: 'Kevin De Bryune',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Kevin De Bryune.png',
         value: 1
     }
 ],
 player_cleansheet = [
     {
         name: 'Ederson',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Ederson.png',
         value: 1
     }
 ],
