6aa67d29efcefcb8da0a8460886bf6103653341e
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-28 21:51:03 +0530

    Update player-stats.js

--- match/season-1/player-stats.js
+++ match/season-1/player-stats.js
@@ -4,6 +4,24 @@ var player_goals = [
         club: 'RS Football Club',
         src: 'assests/player-image/Phil Foden.png',
         value: 1
+    },
+    {
+        name: 'Diogo Jota',
+        club: 'Krish FC',
+        src: 'assests/player-image/Diogo Jota.png',
+        value: 1
+    },
+    {
+        name: 'Rodrigo Moreno',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Rodrigo Moreno.png',
+        value: 1
+    },
+    {
+        name: 'Paulo Dybala',
+        club: 'Annihilation',
+        src: 'assests/player-image/Paulo Dybala.png',
+        value: 1
     }
 ],
 player_assists = [
@@ -11,6 +29,18 @@ player_assists = [
         name: 'Kevin De Bryune',
         club: 'RS Football Club',
         src: 'assests/player-image/Kevin De Bryune.png',
+        value: 1
+    },
+    {
+        name: 'Diogo Jota',
+        club: 'Krish FC',
+        src: 'assests/player-image/Diogo Jota.png',
+        value: 1
+    },
+    {
+        name: 'Kasper Dolberg',
+        club: 'Annihilation',
+        src: 'assests/player-image/Kasper Dolberg.png',
         value: 1
     }
 ],
