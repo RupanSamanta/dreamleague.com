13f20aafe5a2fcb992d8e06be7f0fd14ba079c92
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-10-29 09:20:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function generateMatch(m, no, team1, team2, type, ext) {
     var match = document.createElement('div'),
     div = document.createElement('div');
     match.className = 'match';
-    div.innerHTML = no=="Final"?'Final':'Match '+no;
+    div.innerHTML = no=="Final"?'Final':'Match '+(no+1);
     match.appendChild(div);
     div = document.createElement('div');
     div.innerHTML = '<span>'+team1.shortName+'</span>';
@@ -48,7 +48,7 @@ function generateMatch(m, no, team1, team2, type, ext) {
     }
     else {
         div.innerHTML += `<p id="done">
-            <span>`+results[no-1][0]+`</span><span>-</span><span>`+results[no-1][1]+`</span>
+            <span>`+results[no][0]+`</span><span>-</span><span>`+results[no][1]+`</span>
         </p>`;
         m = m[1];
         match.onclick = function () {
