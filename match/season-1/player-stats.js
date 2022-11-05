aae8a985161d441bcc42d0b219651610129f6180
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-11-05 22:54:39 +0530

    Update player-stats.js

--- match/season-1/player-stats.js
+++ match/season-1/player-stats.js
@@ -46,7 +46,7 @@ var player_goals = [
         club: 'RS Football Club',
         src: 'assests/player-image/Jack Grealish.png',
         value: 1
-    }
+    },
     {
         name: 'Allan Saint-Maximin',
         club: 'Krish FC',
