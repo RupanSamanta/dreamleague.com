322673fc17c7885d50fc19db32b5efd66956956e
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-12-05 20:31:36 +0530

    Create player-stats.js

---
+++ stats/player-stats.js
@@ -0,0 +1,279 @@
+var player_goals = [
+[    {
+        name: 'Phil Foden',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Phil Foden.png',
+        value: 2
+    },
+    {
+        name: 'Diogo Jota',
+        club: 'Krish FC',
+        src: 'assests/player-image/Diogo Jota.png',
+        value: 2
+    },
+    {
+        name: 'Rodrigo Moreno',
+        club: 'Krish FC',
+        src: 'assests/player-image/Rodrigo Moreno.png',
+        value: 1
+    },
+    {
+        name: 'Paulo Dybala',
+        club: 'Annihilation',
+        src: 'assests/player-image/Paulo Dybala.png',
+        value: 2
+    },
+    {
+        name: 'Riyad Mahrez',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Riyad Mahrez.png',
+        value: 2
+    },
+    {
+        name: 'Lionel Messi',
+        club: 'SSFC',
+        src: 'assests/player-image/Lionel Messi.png',
+        value: 3
+    },
+    {
+        name: 'Ángel Correa',
+        club: 'Krish FC',
+        src: 'assests/player-image/Angel Correa.png',
+        value: 1
+    },
+    {
+        name: 'Jack Grealish',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Jack Grealish.png',
+        value: 1
+    },
+    {
+        name: 'Allan Saint-Maximin',
+        club: 'Krish FC',
+        src: 'assests/player-image/Allan Saint-Maximin.png',
+        value: 1
+    },    
+    {
+        name: 'Lionel Messi',
+        club: 'Annihilation',
+        src: 'assests/player-image/Lionel Messi.png',
+        value: 1
+    },
+    {
+        name: 'Raheem Sterling',
+        club: 'SSFC',
+        src: 'assests/player-image/Raheem Sterling.png',
+        value: 1
+    },
+    {
+        name: 'Kylian Mbappé',
+        club: 'SSFC',
+        src: 'assests/player-image/Kylian Mbappe.png',
+        value: 3
+    },
+    {
+        name: 'Mohamed Salah',
+        club: 'Krish FC',
+        src: 'assests/player-image/Mohamed Salah.png',
+        value: 1
+    },
+    {
+        name: 'Rodri',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Rodri.png',
+        value: 1
+    },
+    {
+        name: 'Cristiano Ronaldo',
+        club: 'Krish FC',
+        src: 'assests/player-image/Cristiano Ronaldo.png',
+        value: 1
+    },
+    {
+        name: 'Kevin De Bruyne',
+        club: 'SSFC',
+        src: 'assests/player-image/Kevin De Bruyne.png',
+        value: 2
+    },
+    {
+        name: 'Erling Haaland',
+        club: 'Krish FC',
+        src: 'assests/player-image/Erling Haaland.png',
+        value: 1
+    },
+    {
+        name: 'Erling Haaland',
+        club: 'SSFC',
+        src: 'assests/player-image/Erling Haaland.png',
+        value: 1
+    },
+    {
+        name: 'Erling Haaland',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Erling Haaland.png',
+        value: 1
+    },
+    {
+        name: 'Cristiano Ronaldo',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Cristiano Ronaldo.png',
+        value: 3
+    },
+    {
+        name: 'Ruben Dias',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Ruben Dias.png',
+        value: 1
+    }
+]
+],
+player_assists = [
+[    {
+        name: 'Kevin De Bruyne',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Kevin De Bruyne.png',
+        value: 1
+    },
+    {
+        name: 'Diogo Jota',
+        club: 'Krish FC',
+        src: 'assests/player-image/Diogo Jota.png',
+        value: 1
+    },
+    {
+        name: 'Kasper Dolberg',
+        club: 'Annihilation',
+        src: 'assests/player-image/Kasper Dolberg.png',
+        value: 1
+    },
+    {
+        name: 'Oleksandr Zinchenko',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Oleksandr Zinchenko.png',
+        value: 1
+    },
+    {
+        name: 'Phil Foden',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Phil Foden.png',
+        value: 1
+    },
+    {
+        name: 'Ángel Correa',
+        club: 'Krish FC',
+        src: 'assests/player-image/Angel Correa.png',
+        value: 1
+    },
+    {
+        name: 'Raheem Sterling',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Raheem Sterling.png',
+        value: 1
+    },
+    {
+        name: 'Neymar Jr.',
+        club: 'SSFC',
+        src: 'assests/player-image/Neymar Jr.png',
+        value: 2
+    },
+    {
+        name: 'Kylian Mbappé',
+        club: 'SSFC',
+        src: 'assests/player-image/Kylian Mbappe.png',
+        value: 1
+    },   
+    {
+        name: 'Lionel Messi',
+        club: 'SSFC',
+        src: 'assests/player-image/Lionel Messi.png',
+        value: 1
+    },
+    {
+        name: 'Neymar Jr.',
+        club: 'Krish FC',
+        src: 'assests/player-image/Neymar Jr.png',
+        value: 2
+    },
+    {
+        name: 'Eden Hazard',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Eden Hazard.png',
+        value: 1
+    },
+    {
+        name: 'Vinicius Jr.',
+        club: 'SSFC',
+        src: 'assests/player-image/Vinicius Jr.png',
+        value: 1
+    },
+    {
+        name: 'Riyad Mahrez',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Riyad Mahrez.png',
+        value: 1
+    },
+    {
+        name: 'Bernardo Silva',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Bernardo Silva.png',
+        value: 1
+    },
+    {
+        name: 'Neymar Jr.',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Neymar Jr.png',
+        value: 2
+    },
+    {
+        name: 'Lionel Messi',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Lionel Messi.png',
+        value: 2
+    },
+    {
+        name: 'Virgil van Djik',
+        club: 'SSFC',
+        src: 'assests/player-image/Virgil van Djik.png',
+        value: 1
+    }
+]
+],
+player_cleansheet = [
+[    {
+        name: 'Ederson Moraes',
+        club: 'RS Football Club',
+        src: 'assests/player-image/Ederson.png',
+        value: 5
+    },
+    {
+        name: 'Alisson Becker',
+        club: 'SSFC',
+        src: 'assests/player-image/Alisson.png',
+        value: 1
+    },
+    {
+        name: 'Ederson Moraes',
+        club: 'Krish FC',
+        src: 'assests/player-image/Ederson.png',
+        value: 1
+    },
+    {
+        name: 'Kasper Schmeichel',
+        club: 'Annihilation',
+        src: 'assests/player-image/Kasper Schmeichel.png',
+        value: 1
+    }
+]    
+],
+season1_player_stats = [
+  player_goals, player_assists, player_cleansheet
+
+];
+for(let a = 0; a < season1_player_stats.length; a++)
+   for (let i = 0; i < season1_player_stats[a][1].length; i++)
+      for (let j = 0; j < season1_player_stats[a][1].length; j++)
+        if (season1_player_stats[a][1][i].value > season1_player_stats[a][1][j].value) {
+            var temp = season1_player_stats[a][1][i];
+            season1_player_stats[a][1][i] = season1_player_stats[a][1][j];
+            season1_player_stats[a][1][j] = temp;
+        }
