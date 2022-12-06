5ab959ee8a547130254772a1dccafb09b93c0849
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-12-06 13:27:26 +0530

    Update team-stats.js

--- stats/team-stats.js
+++ stats/team-stats.js
@@ -1,15 +1,19 @@
 const stats_teams = [
   [
     rfc = {
+      name: 'RS Football Club', stadium: 'RS City Stadium', src: 'assests/team/rsfc.png',
       gf: 11, shot: 49, shotTarget: 33, passCompletion: 83, cleansheet: 5
     },
     ann = {
+      name: 'Annihilation', stadium: 'Vasio Stadium', src: 'assests/team/anh.png',
       gf: 5, shot: 10, shotTarget: 4, passCompletion: 47, cleansheet: 1
     },
     kfc = {
+      name: 'Krish FC', stadium: 'Krish Stadium', src: 'assests/team/ssfc.png',
       gf: 8, shot: 21, shotTarget: 18, passCompletion: 71, cleansheet: 1
     },
     sfc = {
+      name: 'SSFC', stadium: 'Laura Stadium', src: 'assests/team/ssfc.png',
       gf: 10, shot: 19, shotTarget: 13, passCompletion: 71, cleansheet: 1
     }
   ]
