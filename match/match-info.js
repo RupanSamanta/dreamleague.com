43cf836816f3cc6f0c6cef8fcbccb69c9fae557a
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-12-09 20:46:02 +0530

    Add files via upload

---
+++ match/match-info.js
@@ -0,0 +1,98 @@
+const match_results = [
+    [],
+    [
+        {
+            date: 'Fri 9 Dec 2022',
+            venue: 'Vasio Stadium',
+            venueUrl: '../../../assests/images/Vasio Stadium.webp',
+            homeTeam: {
+                name: 'Annihilation',
+                shortName: 'ANN',
+                url: 'assests/team/anh.png',
+                form: [1, 4, 1, 2, 3]
+            },
+            awayTeam: {
+                name: 'Krish FC',
+                shortName: 'KFC',
+                url: 'assests/team/ssfc.png',
+                form: [1, 4, 3, 3]
+            },
+            score: [1, 3],
+            goal: {
+                home: [
+                    {
+                        name: 'H. Kane',
+                        time: [43]
+                    }
+                ],
+                away: [                    
+                    {
+                        name: 'C. Ronaldo',
+                        time: [39]
+                    }               
+                ]
+            },
+            assists: {
+                home: [
+                    {
+                        name: 'M. Salah',
+                        time: [43]
+                    }                    
+                ],
+                away: [
+                    {
+                        name: 'L. Modric',
+                        time: [39]
+                    }
+                ]
+            },
+            card:  {
+                home: [],
+                away: []
+            },
+            potm: {
+                firstName: 'Joao',
+                secondName: 'Cancelo',
+                club: '../../../assests/team/kfc.png',
+                url: '../../../assests/player-image/Joao Cancelo.png',
+            },
+            homeLineup: [
+                ['Kasper Schmeichel'],
+                ['Joao Cancelo'],
+                ['Milan Skriniar'],
+                ['Giorgio Chiellini'],
+                ['Kyle Walker'],
+                ['Phil Foden'],
+                ['Luka Modric'],
+                ['Pedri'],
+                ['Kylian Mbappé'],
+                ['Harry Kane'],
+                ['Mohamed Salah']
+            ],
+            awayLineup: [
+                ['Ederson'],
+                ['Joao Cancelo'],
+                ['Virgil van Djik'],
+                ['Thiago Silva'],
+                ['Daniele Rugani'],
+                ['Kevin De Bruyne'],
+                ['Luka Modric'],
+                ['Sadio Mane'],
+                ['Neymar Jr.'],
+                ['Cristiano Ronaldo'],
+                ['Lionel Messi']
+            ],
+            stats: [
+                ['55%', 'Possession', '45%'],
+                ['7', 'Shots', '3'],
+                ['5', 'Shots on Target', '1'],
+                ['1', 'Corners', '0'],
+                ['80%', 'Pass Completion', '79%'],
+                ['1', 'Fouls', '1'], 
+                ['1', 'Yellow Cards', '0'],
+                ['0', 'Red Cards', '0'],
+                ['1', 'Offsides', '1']
+            ]
+        }
+    ]
+]
